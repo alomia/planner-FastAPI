d07e49e7aab4929f07057445c4ccb1d45d97ef4a
alomia <alejandroalomia730@gmail.com>
2023-05-18 20:22:35 -0500

    configuring CORS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from database.connection import Settings
 from routes.users import user_router
@@ -10,6 +11,18 @@ import uvicorn
 app = FastAPI()
 
 settings = Settings()
+
+# register origins
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register routes
 
