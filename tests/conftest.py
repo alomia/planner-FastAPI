8b7c751c56232d7ab33ca9b558cd655ead12ff89
alomia <alejandroalomia730@gmail.com>
2023-06-10 23:07:57 -0500

    setting up our test environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,6 +20,8 @@ async def init_db():
     test_settings = Settings()
     test_settings.DATABASE_URL = "mongodb://localhost:27017/testdb"
 
+    await test_settings.initialize_database()
+
 
 @pytest.fixture(scope="session")
 async def default_client():
